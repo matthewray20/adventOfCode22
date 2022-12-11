b4003993bf7d96348c3cd5bbf3e9a84895140ceb
matthewray20 <mattray99@gmail.com>
2022-12-11 23:52:26 +1100

    squashed day9 bugs

--- day9/rope_bridge.py
+++ day9/rope_bridge.py
@@ -1,7 +1,7 @@
 #!/Users/mattray/miniconda3/bin python
 
 
-FILENAME = 'test.txt'
+FILENAME = 'head_movements.txt'
 
 
 class Knot:
@@ -10,33 +10,36 @@ class Knot:
         self.absolute_y = 0
         self.number = n
         self.tail = None
-        self.locations_visited = []
+        self.locations_visited = [(0, 0)]
     
     def add_tail(self):
         self.tail = Knot(self.number + 1)
     
     def print_all_pos(self):
-        print(f'Knot: {self.number}, x: {self.absolute_x}, y: {self.absolute_y}')
+        #print(f'Knot: {self.number}, x: {self.absolute_x}, y: {self.absolute_y}')
         if self.tail is not None:
             self.tail.print_all_pos()
-        else:
-            print('-------------------------')
             
-    def move(self, direction):
-        if direction == 'U': # move up
-            self.absolute_y += 1
-            print(f'{self.number} moving U')
-        elif direction == 'D': # move down
-            self.absolute_y -= 1
-            print(f'{self.number} moving D')
-        elif direction == 'L': # move left
-            self.absolute_x -= 1
-            print(f'{self.number} moving L')
-        elif direction == 'R': # move right
-            self.absolute_x += 1
-            print(f'{self.number} moving R')
-        else:
-            print(f"Somethign wrong unexpected input: {direction}")
+    def move(self, directions):
+        #print(f'knot {self.number} visited {len(set(self.locations_visited))} locations')
+        for direction in directions:
+            if direction == 'U': # move up
+                self.absolute_y += 1
+                #print(f'{self.number} moving U')
+            elif direction == 'D': # move down
+                self.absolute_y -= 1
+                #print(f'{self.number} moving D')
+            elif direction == 'L': # move left
+                self.absolute_x -= 1
+                #print(f'{self.number} moving L')
+            elif direction == 'R': # move right
+                self.absolute_x += 1
+                #print(f'{self.number} moving R')
+            else:
+                print(f"Somethign wrong unexpected input: {direction}")
+
+        self.locations_visited.append((self.absolute_x, self.absolute_y))
+        #if self.number == 1: print(f'Knot {self.number} saving pos x: {self.absolute_x}, y: {self.absolute_y}')
         
         if self.tail is not None:
             self.tail.update_tail(self.absolute_x, self.absolute_y)
@@ -48,29 +51,25 @@ class Knot:
         relative_y = head_y - self.absolute_y
 
         if relative_x == 0 and relative_y > 1: # move up
-            self.move('U')
+            self.move(['U'])
         elif relative_x == 0 and relative_y < -1: # move down
-            self.move('D')
+            self.move(['D'])
         elif relative_y == 0 and relative_x > 1: # move right
-            self.move('R')
+            self.move(['R'])
         elif relative_y == 0 and relative_x < -1: # move left
-            self.move('L')
+            self.move(['L'])
         elif relative_x >= 1 and relative_y > 1 or relative_x > 1 and relative_y >= 1: # move diag up right
-            self.move('U')
-            self.move('R')
+            self.move(['U', 'R'])
         elif relative_x >= 1 and relative_y < -1 or relative_x > 1 and relative_y <= -1: # move diag down right
-            self.move('D')
-            self.move('R')
+            self.move(['D', 'R'])
         elif relative_x <= -1 and relative_y < -1 or relative_x < -1 and relative_y <= -1: # move diag down left
-            self.move('D')
-            self.move('L')
+            self.move(['D', 'L'])
         elif relative_x <= -1 and relative_y > 1 or relative_x < -1 and relative_y >= 1: # move diag up left
-            self.move('U')
-            self.move('L')
+            self.move(['U', 'L'])
         else:
             #print(f"touching should lead here! Rel x: {relative_x}, rel y: {relative_y}")
             pass
-        self.locations_visited.append((self.absolute_x, self.absolute_y))
+        
     
     def end(self, n):
         if self.number == n:
@@ -78,41 +77,44 @@ class Knot:
         else:
             self.tail.end(n)
 
-            
 
-def part1():
+def bridge_sim(n_knots):
     head = Knot(0)
     current = head
-    for i in range(1, 10):
-        current.add_tail()
-        current = current.tail
+    if n_knots > 1:
+        for i in range(1, n_knots):
+            current.add_tail()
+            current = current.tail
     with open(FILENAME, 'r') as f:
         for line in f:
             line = line.strip()
             line = line.split(' ')
             n = int(line[1])
             for _ in range(n):
-                direction = line[0]
+                direction = [line[0]]
                 head.move(direction)
                 head.print_all_pos()
-            print()
-    head.end(9)
+    head.end(n_knots-1)
 
+            
 
-
-
-
-
-
-
-
-
-
-
+def part1():
+    print('part1:')
+    bridge_sim(2)
 
 
 def part2():
-    pass
+    print('part2:')
+    bridge_sim(10)
+
+
+
+
+
+
+
+
+
 
 
 
