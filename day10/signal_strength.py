df01ac7fe5fb02632429a874409911e15483a0b1
matthewray20 <mattray99@gmail.com>
2022-12-12 00:52:47 +1100

    day10 getting closer

--- day10/signal_strength.py
+++ day10/signal_strength.py
@@ -35,10 +35,34 @@ def part1():
 
 def part2():
     with open(FILENAME, 'r') as f:
+        output_str = ''
+        timestamp = -1
+        registor = 1
         for line in f:
             line = line.strip()
             instruction = line[:4]
-            
+            if instruction == 'noop':
+                to_add = 0
+            elif instruction == 'addx':
+                to_add = int(line[5:]) 
+                timestamp += 1
+                pixel_x = timestamp % 40
+                if pixel_x == 0:
+                    output_str += '\n'
+                output_str += '#' if pixel_x in [x for x in range(registor-1, registor+2)] else '.'
+            timestamp += 1
+            pixel_x = timestamp % 40
+            if pixel_x == 0:
+                print('new line:', timestamp)
+                output_str += '\n'
+            output_str += '#' if pixel_x in [x for x in range(registor-1, registor+2)] else '.'
+            registor += to_add
+    print(output_str)
+    lens = output_str.split()
+    for d in lens:
+        print(len(d))
+
+
 
 
 
